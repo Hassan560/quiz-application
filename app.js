f800924287966b2acce3ff8fef1a1fb9f69d04dc
syedhassanhussain <syedhassanhussain001@gmail.com>
2021-08-12 17:26:00 +0500

    complete

---
+++ app.js
@@ -0,0 +1,91 @@
+function submit() {
+  var score = "";
+  var write_answer = document.getElementById(1);
+  var q1_answer = document.getElementById(2);
+  var q1_answer = document.getElementById(3);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 1 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(4);
+  var q2_answer = document.getElementById(5);
+  var q2_answer = document.getElementById(6);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 2 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(8);
+  var q3_answer = document.getElementById(7);
+  var q3_answer = document.getElementById(9);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 3 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(12);
+  var q4_answer = document.getElementById(11);
+  var q4_answer = document.getElementById(10);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 4 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(13);
+  var q5_answer = document.getElementById(14);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 5 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(16);
+  var q6_answer = document.getElementById(15);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 6 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(17);
+  var q7_answer = document.getElementById(18);
+  var q7_answer = document.getElementById(19);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 7 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(21);
+  var q8_answer = document.getElementById(20);
+  var q8_answer = document.getElementById(22);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 8 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(23);
+  var q9_answer = document.getElementById(24);
+  var q9_answer = document.getElementById(25);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 9 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+  var write_answer = document.getElementById(27);
+  var q10_answer = document.getElementById(26);
+  var q10_answer = document.getElementById(28);
+  if (write_answer.checked == true) {
+    score++;
+    alert("Question 10 is write");
+  } else {
+    alert("your answer is wrong");
+  }
+}
